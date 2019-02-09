dcdb27cfb7efa8930289c570ed1cbdd721e201d4
Din Cheng <din1030@gmail.com>
2019-02-10 02:27:12 +0800

    First commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+	var mySwiper = new Swiper('#main-slider', {
+		loop: true,
+		autoplay: {
+			delay: 3500,
+		},
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+	})
+
+	$(window).scroll(function(event) {
+		if (!$('#float-line').hasClass('animate') && $('#join-block').visible) {
+			$('#float-line').addClass('animate');
+		}
+	});
+
+});
+
+// 判斷元件是否出現在 viewport
+(function($) {
+	/**
+	 * Copyright 2012, Digital Fusion
+	 * Licensed under the MIT license.
+	 * http://teamdf.com/jquery-plugins/license/
+	 *
+	 * @author Sam Sehnert
+	 * @desc A small plugin that checks whether elements are within
+	 *     the user visible viewport of a web browser.
+	 *     only accounts for vertical position, not horizontal.
+	 */
+
+	$.fn.visible = function(partial) {
+
+		var $t = $(this),
+			$w = $(window),
+			viewTop = $w.scrollTop(),
+			viewBottom = viewTop + $w.height(),
+			_top = $t.offset().top,
+			_bottom = _top + $t.height(),
+			compareTop = partial === true ? _bottom : _top,
+			compareBottom = partial === true ? _top : _bottom;
+
+		return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+	};
+
+})(jQuery);

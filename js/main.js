b8341b5d147b64bcf45a13a852f7a1715ec94b97
Din Cheng <din1030@gmail.com>
2019-02-13 13:55:36 +0800

    0213 ver

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+	$("#scroll-down a").click(function(event) {
+		$(document).scrollTo($('#scroll-down-target'), 500);
+	});
 	var mySwiper = new Swiper('#main-slider', {
 		loop: true,
 		autoplay: {
@@ -16,7 +19,86 @@ $(document).ready(function() {
 			prevEl: '.swiper-button-prev',
 		},
 
-	})
+	});
+
+	var images = [
+		'../img/svg/stage1.svg',
+		'../img/svg/stage2.svg',
+		'../img/svg/stage3.svg'
+	];
+	// TweenMax can tween any property of any object. We use this object to cycle through the array
+	var obj = {
+		curImg: 0
+	};
+	var content = [
+		"我們致力於打造完善的<br>資料科學應用方案",
+		"讓每個領域的專家<br>都能輕鬆運用",
+		"以資料為燃料<br>打造下一個改變世界的引擎"
+	];
+
+	// create tween
+	var tween = TweenMax.to(obj, 0.5, {
+		curImg: images.length - 1, // animate propery curImg to number of images
+		roundProps: "curImg", // only integers so it can be used as an array index
+		// repeat: 3, // repeat 3 times
+		immediateRender: true, // load first image automatically
+		ease: Linear.easeNone, // show every image the same ammount of time
+		onUpdate: function() {
+			$("#scroll-animation img").attr("src", images[obj.curImg]); // set the image source
+			$("#parallax-text").html(content[obj.curImg]);
+		}
+	});
+	// init controller
+	var controller = new ScrollMagic.Controller();
+
+	// create a scene
+	var scene = new ScrollMagic.Scene({
+			triggerElement: '#animation-trigger',
+			duration: 700, // the scene should last for a scroll distance of 100px
+			triggerHook: 0.05
+		})
+		.setTween(tween)
+		.setPin("#animation-wrapper")
+		.addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
+
+	var originalSrc, $teamPic;
+	var hoverPhoto = [
+		'roger-hover.png',
+		'ken-hover.png',
+		'owl-hover.svg',
+		'owl-hover.svg'
+	];
+	$('.photo-wrapper').mouseenter(function(event) {
+		var no = $(this).data('no') - 1;
+		$teamPic = $(this).find('img').eq(0);
+		originalSrc = $teamPic.attr('src');
+		$teamPic.attr('src', 'img/team/' + hoverPhoto[no]);
+	});
+	$('.photo-wrapper').mouseleave(function(event) {
+		$teamPic.attr('src', originalSrc);
+	});
+
+	particlesJS.load("particles-overlay", "./js/particle.json", function() {
+		console.log('callback - particles.js config loaded');
+	});
+	// var count_particles, stats, update;
+	// stats = new Stats;
+	// stats.setMode(0);
+	// stats.domElement.style.position = 'absolute';
+	// stats.domElement.style.left = '0px';
+	// stats.domElement.style.top = '0px';
+	// document.body.appendChild(stats.domElement);
+	// count_particles = document.querySelector('.js-count-particles');
+	// update = function() {
+	// 	stats.begin();
+	// 	stats.end();
+	// 	if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+	// 		count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+	// 	}
+	// 	requestAnimationFrame(update);
+	// };
+	// requestAnimationFrame(update);;
 
 	$(window).scroll(function(event) {
 		if (!$('#float-line').hasClass('animate') && $('#join-block').visible) {
